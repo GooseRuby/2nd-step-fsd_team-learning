cbbf477054d6841940ea30b36451f25244a45081
Goose <mifistosilver@ya.ru>
2020-09-17 08:33:27 +0700

    Подготовка к установке webpack

---
+++ Post.js
@@ -0,0 +1,13 @@
+class Post {
+  constructor(title) {
+    this.title = title
+    this.date = new Date()
+  }
+
+  toSring() {
+    return JSON.stringify({
+      title: this.title,
+      date: this.date.toJSON()
+    })
+  }
+}
